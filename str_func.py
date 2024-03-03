7421f38c475ddd27f0f7cee0d3105ff755976283
luuvmane <162039016+luuvmane@users.noreply.github.com>
2024-03-03 23:49:09 +0300

    Дополнение функционала

--- str_func.py
+++ str_func.py
@@ -1,4 +1,8 @@
 def uppercase_string(input_string):
     """Преобразует строку в заглавные буквы."""
-
     return input_string.upper()
+
+
+def capitalize_words(input_string):
+    """Делает заглавными первые буквы каждого слова в строке."""
+    return input_string.title()
